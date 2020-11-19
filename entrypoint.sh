1ecf8139dbd2a39882a5576bc8df345b1cc96c24
vagrant <vagrant@k8s-dev>
2020-11-19 22:30:23 +0800

    k8s-push-test

---
+++ entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PREFIX="${PREFIX:-default}"
+COMMITHASH="${COMMITHASH:- }"
+COMMITLOG="${COMMITLOG:- }"
+IPS=$(hostname -I)
+
+cat <<EOF > /var/www/html/index.html
+Hostname is ${PREFIX}_${HOSTNAME}
+Latest Commit Hash is ${PREFIX}_${COMMITHASH}
+Latest Commit Log is ${PREFIX}_${COMMITLOG}
+IP address list are ${PREFIX}_${IPS}
+Datetime is $(date)
+Welcome to K8s Dennis!
+THANK YOU!
+I LOVE YOU!
+NAMASTE!
+PEACE!
+ZEN!
+ZEROLIMIT!
+I AM SORRY!
+EOF
+
+#Generate a new index.html under /var/www/html
+exec "$(which nginx)" -c /etc/nginx/nginx.conf -g "daemon off;"
